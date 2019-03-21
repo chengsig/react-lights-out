00d7ab749481209b6881ae4ed413b501084ffb99
Chengsi <gaochengsi@gmail.com>
2019-03-20 22:02:16 -0700

    initial commit

---
+++ src/Game.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Board from "./Board"
+
+class Game extends Component {
+  render(){
+    return(
+        <Board/>
+    )
+  }
+}
+
+export default Game
